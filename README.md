23b29653f7ef397862aebb4b3a195610105348a8
alan <alanvega002@gmail.com>
2021-01-16 10:47:58 -0800

    improved project file structure

---
+++ README.md
@@ -0,0 +1,9 @@
+# IOT Logger on the the Tangle
+
+---
+
+Multi-use tool for IOT devices to interact with the Tangle Network for receiving and sending encrypted data
+
+- Built: Go
+- Network: Tangle
+- Protocol: IOTA
--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"encoding/json"
+
 	"github.com/iotaledger/iota.go/api"
 
 	"fmt"
@@ -8,17 +10,26 @@ import (
 
 var endpoint = "http://node.iotadev.org:14265"
 
+type IotaAPI struct{
+	API: api.Api
+}
+
 // API connects to API of a IRI node on the Tangle network
-func API() {
+func API() IotaAPI {
 
 	// compose a new API instance
-	api, err := api.ComposeAPI(api.HTTPClientSettings{URI: endpoint})
+	iotaAPI, err := api.ComposeAPI(api.HTTPClientSettings{URI: endpoint})
 	must(err)
 
-	nodeInfo, err := api.GetNodeInfo()
+	nodeInfo, err := iotaAPI.GetNodeInfo()
+	must(err)
+
+	pretty, err := json.MarshalIndent(nodeInfo, "", "    ")
 	must(err)
 
 	fmt.Println("latest milestone index:", nodeInfo.LatestMilestoneIndex)
+	fmt.Println(string(pretty))
+	return iotaAPI.api
 }
 
 func must(err error) {
